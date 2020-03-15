d007ea7ede657a2d3d3c99e319d44b3e2e55059c
RY <raviyadav5951@gmail.com>
2020-03-15 13:26:21 +0530

    completed data binding

--- app/src/main/java/com/askfortricks/unittest/view/AnimalListAdapter.kt
+++ app/src/main/java/com/askfortricks/unittest/view/AnimalListAdapter.kt
@@ -3,10 +3,14 @@ package com.askfortricks.unittest.view
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.askfortricks.unittest.R
 import com.askfortricks.unittest.databinding.ItemAnimalBinding
 import com.askfortricks.unittest.model.Animal
+
+//1.Data Binding the row items
+//2.Data Binding the function calls(see itemclick listener)
 
 class AnimalListAdapter(val animalList:ArrayList<Animal>):RecyclerView.Adapter<AnimalListAdapter.AnimalViewHolder>() {
 
@@ -20,10 +24,12 @@ class AnimalListAdapter(val animalList:ArrayList<Animal>):RecyclerView.Adapter<A
 
     override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
         holder.view.animal=animalList[position]
-//        holder.itemView.animalLayout.setOnClickListener {
-//            val action=ListFragmentDirections.actionDetail(animalList[position])
-//            Navigation.findNavController(holder.itemView).navigate(action)
-//        }
+
+        //we can also use databinding for click listener but its a developer choice
+        holder.view.animalLayout.setOnClickListener {
+            val action=ListFragmentDirections.actionDetail(animalList[position])
+            Navigation.findNavController(holder.itemView).navigate(action)
+        }
     }
 
     fun updateAnimalList(newAnimalList:List<Animal>){
